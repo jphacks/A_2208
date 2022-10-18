bd5991ea3801608a712d0e07df46b789f561c4a2
kujiro <kantake1969@gmail.com>
2022-10-18 21:27:29 +0900

    docs生成

--- src/frontend/src/App.svelte
+++ src/frontend/src/App.svelte
@@ -4,7 +4,7 @@
 
 <main>
 	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	
 </main>
 
 <style>
@@ -15,16 +15,5 @@
 		margin: 0 auto;
 	}
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+	
 </style>
