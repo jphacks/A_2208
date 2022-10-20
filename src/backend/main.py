d74744c5a454d8f5307c9f4a7bd1f3dcec79162f
Reso <84968076+kake-r@users.noreply.github.com>
2022-10-20 22:13:58 +0900

    Token verification and Authentication Functions

--- src/backend/main.py
+++ src/backend/main.py
@@ -1,8 +1,23 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from dotenv import load_dotenv
+import firebase_admin
+from fb_setting import get_current_user
 
 app = FastAPI()
+
+load_dotenv()
+default_app = firebase_admin.initialize_app()
+
+
 
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.get("/sample")
+async def id(user_data=Depends(get_current_user)):
+    uid = user_data["uid"]
+    print(uid)
+    return [uid]
