52575be147184e7ce77dbe77dfce13cc2e8ce2f6
Today017 <yesterday998244353@gmail.com>
2022-10-20 16:06:04 +0900

    編集ページ雛型

--- src/frontend/src/App.svelte
+++ src/frontend/src/App.svelte
@@ -8,17 +8,50 @@
 
 	onMount(() => {
 		document.getElementById("preview").innerHTML = `flowchart TB
-		alpha{"料理開始"} --> A
+		alpha{"料理開始"}
+		A["じゃがいもを洗う"]
+		B["にんじんの上端、下端を切り落とす"]
+		C["玉ねぎの上端、下端を切り落とす"]
+		D["にんじんを洗う"]
+		E["玉ねぎの皮をむく"]
+		F["ピーラーで皮をむく"]
+		G["豚肩肉を一口大に切る"]
+		H["にんじんを半月切りにする"]
+		I["じゃがいもを乱切りをする"]
+		J["玉ねぎをくし切りにする"]
+		K["厚手の鍋にサラダ油を入れて熱する"]
+		L["焦がさないように中火で炒める"]
+		M["鍋に水を入れて沸騰させる"]
+		N[/"アクが出る"/]
+		O["取り除く"]
+		P["弱火or中火で20分煮込む"]
+		Q["火を止める"]
+		R["ルーを入れて溶かす"]
+		S["再度火をつけ弱火で5分煮込む"]
+
+		alpha --> A
 		alpha --> B
-		A["<じゃがいも>・<にんじん>を洗う"] --> C
-		B["<たまねぎ>を皮をむいて洗う"] --> E
-		C["ピーラーで<じゃがいも>・<にんじん>を皮をむく"] --> D
-		D["<にんじん>を1cm幅の<<いちょう切り>>にする"] --> G
-		E["<たまねぎ>を薄切りにする"] --> G
-		F["厚手の鍋にサラダ油を入れて熱する"] --> G
-		G["豚コマ肉・じゃがいも・にんじん・たまねぎをいれ、焦がさないように炒める。"] --> H
-		
-		click alpha call click_alpha()
+		alpha --> C
+		B --> D
+		A --> F
+		D --> F
+		C --> E
+		F --> H
+		F --> I
+		E --> J
+		G --> L
+		H --> L
+		I --> L
+		J --> L
+		K --> L
+		L --> M
+		M --> N
+		N --はい --> O
+		N --いいえ --> P
+		O --> P
+		P --> Q
+		Q --> R
+		R --> S
 		`;
 		mermaid.initialize({ startOnLoad: true, securityLevel: "loose" });
 	});
@@ -27,16 +60,20 @@
 </script>
 
 <main>
-	<div id="makingNode">
+	<div id="makeFlowChart">
+		<div id="preview" class="mermaid" />
+		<div class="nodeButtonArea">
+			<!-- ボタン用のいい感じの画像を作成する -->
+			<button class="nodeButton">矢印を追加する</button>
+			<button class="nodeButton">矢印を編集する</button>
+			<button class="nodeButton">矢印を削除する</button>
+		</div>
+	</div>
+	<div id="makeNode">
 		<p>料理工程の作成</p>
-		<textarea>料理の工程を書き込んでください</textarea>
+		<textarea placeholder="料理の工程を書き込んでください"></textarea>
 		<button>登録する</button>
 	</div>
-	<div id="makingEdge">
-		
-	</div>
-
-	<div id="preview" class="mermaid" />
 </main>
 
 <style>
@@ -44,10 +81,25 @@
 		width: 100%;
 	}
 
-	main {
+	#preview {
+		text-align: center;
+	}
+
+	.nodeButtonArea {
+		text-align: right;
+	}
+	.nodeButton {
+		width: 100px;
+		display: inline-flex;
+		flex-direction: column;
+	}
+
+	/* main {
 		text-align: center;
 		padding: 1em;
 		width: 80vh;
 		margin: 0 auto;
-	}
+	} */
+
+	
 </style>
