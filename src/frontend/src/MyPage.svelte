3291764b87fd5d1c767edfdd0b1a5a6c900d18f3
inebriety <gougeren130@gmail.com>
2022-11-09 23:32:53 +0900

    マイページ；ボックスのマージン調整途中

--- src/frontend/src/MyPage.svelte
+++ src/frontend/src/MyPage.svelte
@@ -6,15 +6,17 @@
 	<div id="content">
 		<header class="row1 col1">
 			<img id="logo" src="./img/cookingitlogo.png" alt="" />
-			<button>レシピ作成</button>
+			<button>
+				<img src="" alt="">
+			</button>
 			<button>通知</button>
 		</header>
 		<main class="row2 col1">
 			<div id="searchMyRecipe">
 				<button>投稿済みレシピ一覧</button>
 				<form id="searchBox" method="get">
-					<input placeholder="レシピ名を検索" />
-					<input type="submit" value="検索" />
+					<input id="searcher" placeholder="レシピ名を検索" />
+					<button id="searchButton"><img id="searchbutton" src=".\img\searchIcon.png" alt=""></button>
 				</form>
 			</div>
 			<div id="calenderWindow">
@@ -23,6 +25,14 @@
 			<div id="myRecipeListWindow">
 
 			</div>
+			<div class="box">
+				カレンダーもしくは草
+			</div>
+			<div class="box2">
+				レシピ
+				<p>作った数に応じて増やしたい</p>
+			</div>
+
 		</main>
 	</div>
 </div>
@@ -50,5 +60,43 @@
 	#logo {
 		width: auto;
 		height: 5rem;
+	}
+
+	#searchMyRecipe{
+		font-family: "Stick";
+	}
+	#searcher{
+		border-radius: 10px;
+	}
+	#searchBox{
+		width: 20px;
+	}
+	#searchButton{
+		background: transparent;
+		border: transparent;
+	}
+	#searchbutton{
+		width: 20px;
+	}
+	.box {
+    padding-bottom: 20px;
+    margin-bottom: 20px;
+    font-weight: bold;
+    color: #6091d3;/*文字色*/
+    background: #D9D9D9;
+    border-radius: 10px;/*角の丸み*/
+	width: 500px;
+	height: 200px;
+	}
+	.box2 {
+	padding: 0.5em 1em;
+	margin: 1rem 1rem 1rem 1rem;
+    font-weight: bold;
+    color: #6091d3;/*文字色*/
+    background: #D9D9D9;
+    border-radius: 10px;/*角の丸み*/
+	width: 500px;
+	height: 300px;
+
 	}
 </style>
