fe05115be32ad9c1e6c4a02ad4cf5c60cd233e92
Reso <84968076+kake-r@users.noreply.github.com>
2022-11-13 01:07:30 +0900

    API化 wip

--- src/backend/github.py
+++ src/backend/github.py
@@ -24,45 +24,57 @@ def get_user_data():
     
 
 
-def make_repo(recipe_name):
+def make_repo(recipename):
     headers = {
         'Accept': 'application/vnd.github+json',
         'Authorization': 'Bearer '+GITHUB_OAUTH_TOKEN,
         'Content-Type': 'application/x-www-form-urlencoded',
     }
 
-    data = '{"name":"CookinGit-recipe-'+recipe_name+'","description":"This is CookinGit test repo!","homepage":"https://github.com","private":false,"auto_init":true}'
+    data = '{"name":"CookinGit-recipe-'+recipename+'","description":"This is CookinGit test repo!","homepage":"https://github.com","private":false,"auto_init":true}'
 
 
     response = requests.post('https://api.github.com/user/repos', headers=headers, data=data)
 
+#tokenの持ち主がowner前提
 def make_new_recipe_file(recipename, mddata):
     recipe_repo_list = get_recipe_repo_list()
+    owner = get_user_data()
 
-    if "CookinGit-recipe-"+recipename in recipe_repo_list:
-        update_recipe_file(recipename, mddata)
+    if not "CookinGit-recipe-"+recipename in recipe_repo_list:
+        make_repo(recipename)
+
+    recipe_repo_detail_json = get_recipe_repo_detail(owner, recipename)
+
+    if 'sha' in recipe_repo_detail_json:
+        print(recipe_repo_detail_json)
+        update_recipe_file(owner, recipename, mddata)
         
-    commit_message = "first commit"
+    if not 'sha' in recipe_repo_detail_json:
+        commit_message = "first commit"
 
-    github_user_name = get_user_data()
-    repo = "CookinGit-recipe-"+recipename
-    path = repo+".md"
+        github_user_name = get_user_data()
+        repo = "CookinGit-recipe-"+recipename
+        path = repo+".md"
 
-    headers = {
-        'Accept': 'application/vnd.github+json',
-        'Authorization': 'Bearer '+GITHUB_OAUTH_TOKEN,
-    }
+        headers = {
+            'Accept': 'application/vnd.github+json',
+            'Authorization': 'Bearer '+GITHUB_OAUTH_TOKEN,
+        }
 
-    contents_data = mddata
+        contents_data = mddata
 
-    data = '{"message":"'+commit_message+'","content":"'+contents_data+'"}'
+        data = '{"message":"'+commit_message+'","content":"'+contents_data+'"}'
 
 
-    response = requests.put('https://api.github.com/repos/'+github_user_name+'/'+repo+'/contents/'+path, headers=headers, data=data)
-    print("Status Code", response.status_code)
+        response = requests.put('https://api.github.com/repos/'+github_user_name+'/'+repo+'/contents/'+path, headers=headers, data=data)
+        print("Status Code", response.status_code)
+
+        status_code = response.status_code
+        return status_code
 
 def update_recipe_file(owner, recipename, mddata):
-    recipe_repo_detail_json = recipe_repo_detail(owner, recipename)
+    recipe_repo_detail_json = get_recipe_repo_detail(owner, recipename)
     recipe_repo_sha = recipe_repo_detail_json['sha']
 
     repo = "CookinGit-recipe-"+recipename
@@ -103,7 +115,7 @@ def get_recipe_repo_list():
 
 
 
-def recipe_repo_detail(owner, recipename):
+def get_recipe_repo_detail(owner, recipename):
     # リポジトリからファイルを取得
     headers = {
     'Accept': 'application/vnd.github+json',
@@ -118,13 +130,21 @@ def recipe_repo_detail(owner, recipename):
 
     recipe_repo_detail_json = response.json()
 
+    #print(recipe_repo_detail_json)
     # print(response.json()['sha'])
     # print(response.json()['content'])
 
     return recipe_repo_detail_json
 
-
+def aaa():
+    print(GITHUB_OAUTH_TOKEN)
 
 # make_new_recipe_file("test1", "bXkgbmV3IGZpbGUgY29udGVudHM=")
-# recipe_repo_detail("kake-r", "test1")
+# get_recipe_repo_detail("kake-r", "test1")
 # update_recipe_file("kake-r", "test1", "bXkgdXBkYXRlZCBmaWxlIGNvbnRlbnRz")
+
+#make_repo("test2")
+#make_new_recipe_file("test2", "bXkgdXBkYXRlZCBmaWxlIGNvbnRlbnRz")
+#update_recipe_file("kake-r", "test2", "bXkgbmV3IGZpbGUgY29udGVudHM=")
+
+#get_recipe_repo_detail("kake-r", "test2")
